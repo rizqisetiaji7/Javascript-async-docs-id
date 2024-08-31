596243a9fa95856d11533b05f65a281bbed5a616
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2024-08-31 10:01:51 +0700

    Move async content to async directory

--- constants/Links.mjs
+++ constants/Links.mjs
@@ -1,62 +1,62 @@
 export const SIDEBAR_LINKS = {
-	'/docs/': [
+	'/docs/async/': [
 		{
 			text: 'Intro',
 			items: [
-				{ text: 'Agenda Belajar', link: '/docs/' },
-				{ text: 'Pengenalan Async', link: '/docs/intro/async-introduction' },
-				{ text: 'Async method setTimeout()', link: '/docs/intro/settimeout' },
-				{ text: 'Async method setInterval()', link: '/docs/intro/setinterval' },
+				{ text: 'Agenda Belajar', link: '/docs/async/' },
+				{ text: 'Pengenalan Async', link: '/docs/async/intro/async-introduction' },
+				{ text: 'Async method setTimeout()', link: '/docs/async/intro/settimeout' },
+				{ text: 'Async method setInterval()', link: '/docs/async/intro/setinterval' },
 			]
 		},
 		{
 			text: 'AJAX',
 			collapsed: false,
 			items: [
-				{ text: 'Definisi', link: '/docs/ajax/definisi-ajax' },
-				{ text: 'Response Status', link: '/docs/ajax/response-status' },
-				{ text: 'AJAX State', link: '/docs/ajax/state' },
-				{ text: 'Mengirim Data', link: '/docs/ajax/send-data' },
-				{ text: 'URL Search Params', link: '/docs/ajax/url-search-params' },
-				{ text: 'Form Data', link: '/docs/ajax/form-data' },
-				{ text: 'Upload File', link: '/docs/ajax/upload-file' },
+				{ text: 'Definisi', link: '/docs/async/ajax/definisi-ajax' },
+				{ text: 'Response Status', link: '/docs/async/ajax/response-status' },
+				{ text: 'AJAX State', link: '/docs/async/ajax/state' },
+				{ text: 'Mengirim Data', link: '/docs/async/ajax/send-data' },
+				{ text: 'URL Search Params', link: '/docs/async/ajax/url-search-params' },
+				{ text: 'Form Data', link: '/docs/async/ajax/form-data' },
+				{ text: 'Upload File', link: '/docs/async/ajax/upload-file' },
 			]
 		},
 		{
 			text: 'Promise',
 			collapsed: false,
 			items: [
-				{ text: 'Definisi', link: '/docs/promise/definisi-promise' },
-				{ text: 'Promise Method', link: '/docs/promise/promise-method' },
-				{ text: 'Promise Static Method', link: '/docs/promise/static-method' },
+				{ text: 'Definisi', link: '/docs/async/promise/definisi-promise' },
+				{ text: 'Promise Method', link: '/docs/async/promise/promise-method' },
+				{ text: 'Promise Static Method', link: '/docs/async/promise/static-method' },
 			]
 		},
 		{
 			text: 'Fetch API',
 			collapsed: false,
 			items: [
-				{ text: 'Definisi', link: '/docs/fetch-api/definisi-fetch-api' },
-				{ text: 'Request Object', link: '/docs/fetch-api/request' },
-				{ text: 'Response Object', link: '/docs/fetch-api/response' },
+				{ text: 'Definisi', link: '/docs/async/fetch-api/definisi-fetch-api' },
+				{ text: 'Request Object', link: '/docs/async/fetch-api/request' },
+				{ text: 'Response Object', link: '/docs/async/fetch-api/response' },
 			]
 		},
 		{
 			text: 'Async Await',
 			collapsed: false,
 			items: [
-				{ text: 'Definisi', link: '/docs/async-await/definisi-async-await' },
-				{ text: 'Async', link: '/docs/async-await/async' },
-				{ text: 'Await', link: '/docs/async-await/await' },
-				{ text: 'Error Handling', link: '/docs/async-await/error-handling' },
+				{ text: 'Definisi', link: '/docs/async/async-await/definisi-async-await' },
+				{ text: 'Async', link: '/docs/async/async-await/async' },
+				{ text: 'Await', link: '/docs/async/async-await/await' },
+				{ text: 'Error Handling', link: '/docs/async/async-await/error-handling' },
 			]
 		},
 		{
 			text: 'Web Worker',
 			collapsed: false,
 			items: [
-				{ text: 'Javascript Thread', link: '/docs/web-worker/thread' },
-				{ text: 'Definisi', link: '/docs/web-worker/definisi-web-worker' },
-				{ text: 'Worker Object', link: '/docs/web-worker/worker-object' },
+				{ text: 'Javascript Thread', link: '/docs/async/web-worker/thread' },
+				{ text: 'Definisi', link: '/docs/async/web-worker/definisi-web-worker' },
+				{ text: 'Worker Object', link: '/docs/async/web-worker/worker-object' },
 			]
 		},
 	],
@@ -71,7 +71,12 @@ export const SIDEBAR_LINKS = {
 
 export const NAV_LINKS = [
 	{ text: 'Web API', link: 'https://developer.mozilla.org/en-US/docs/Web/API' },
-	{ text: 'Dokumentasi', link: '/docs/' },
+	{ 
+		text: 'Dokumentasi', 
+		items: [
+			{ text: 'JavaScript Async', link: '/docs/async' }
+		],
+	},
 	{ text: 'Referensi', link: '/references/' }
 ]
 
