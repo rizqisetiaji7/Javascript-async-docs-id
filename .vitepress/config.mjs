68cee50aac653434a4327a61bb90f36b53fe22c2
Rizqi Setiaji <rizqisetiaji9@gmail.com>
2024-08-31 10:28:12 +0700

    Change title & description

--- .vitepress/config.mjs
+++ .vitepress/config.mjs
@@ -4,8 +4,8 @@ import { NAV_LINKS, SIDEBAR_LINKS, SOCIAL_LINKS } from '../constants/Links.mjs'
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
   lang: 'id-ID',
-  title: "Async Docs",
-  description: "Javascript ASYNC Dokumentasi Bahasa Indonesia",
+  title: "JavaScript Docs",
+  description: "JavaScript Dokumentasi Bahasa Indonesia",
   cleanUrls: true,
   markdown: {
     theme: {
